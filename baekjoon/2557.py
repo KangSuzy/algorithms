4740cedbfefb4c1585aff71e66e9f67b010b1b38
suzy <astnwl321@gmail.com>
2018-11-25 16:03:36 +0900

    Hello World

---
+++ baekjoon/2557.py
@@ -0,0 +1,18 @@
+"""
+Hello World 성공
+시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
+1 초	128 MB	126947	59961	47496	47.831%
+문제
+Hello World!를 출력하시오.
+
+입력
+없음
+
+출력
+Hello World!를 출력하시오.
+
+예제 입력 1
+예제 출력 1
+Hello World!
+"""
+print("Hello World!")
