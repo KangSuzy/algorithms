42c7465700219ea050f7b029c1af248143b60548
suzy <astnwl321@gmail.com>
2018-11-25 16:16:14 +0900

    Dogs

---
+++ baekjoon/10172.py
@@ -0,0 +1,26 @@
+"""
+Dogs 성공
+시간 제한	메모리 제한	제출	정답	맞은 사람	정답 비율
+1 초	256 MB	57469	19551	17909	37.891%
+문제
+Print the ascii dog as shown below.
+
+입력
+none.
+
+출력
+Print the picture as shown below. The long horizontal line __ is comprised of two underscore characters, and the dog’s nose is a zero.
+
+예제 입력 1
+예제 출력 1
+|\_/|
+|q p|   /}
+( 0 )'''\
+|"^"`    |
+||_/=\\__|
+"""
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
